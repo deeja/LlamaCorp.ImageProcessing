22b10e6fb20837bb4302ebcb142d96ef60c8bb98
deeja@users.noreply.github.com <deeja@users.noreply.github.com>
2020-06-16 22:11:09 +0100

    Move Projects and add test project + image

---
+++ test/LLamaCorp.ImageProcessing.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LLamaCorp.ImageProcessing.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
